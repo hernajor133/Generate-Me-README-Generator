87f6f0b826cd38db3b86282634db6eb11936fa98
Jordi <damlabulbul26@gmail.com>
2022-03-18 00:37:44 -0700

    added packages  and user questions for this project

--- script.js
+++ script.js
@@ -2,3 +2,84 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown');
+
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Use a valid title!");
+            }
+            return true;
+            
+        }
+    },
+
+    {
+        type: 'input',
+        message: 'Describe your project',
+        name: 'Description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Use a valid description!");
+            }
+            return true;
+            
+        }
+    },
+
+    {
+        type: 'input',
+        message: "Describe steps required for installation.",
+        name: 'instalation',
+    },
+
+    {
+        type: 'input',
+        message: "Describe usage of this project.",
+        name: 'usage',
+        default: 'Project Usage',
+    },
+
+    {
+        type: 'list',
+        message: "Choose a license for this project.",
+        choices: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'none'],
+        name: 'license',
+    },
+
+    {
+        type: 'input',
+        message: "Describe any contributors, or how any user can contribute to this project.",
+        name: 'contributing'
+    },
+
+    {
+        type: 'input',
+        message: "If applicable provide tests for this project.",
+        name: 'tests',
+    },
+
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'GitHub',
+        default: 'Project GitHub',
+    },
+
+    {
+        type: 'input',
+        message: "Enter email address.",
+        name: 'email',
+        default: 'email@example.com',
+    },
+
+    
+];
+
+
